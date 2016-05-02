0b070e7c227381a138feba2e01e4d6605983b15f
downer36 <onnicks36@gmail.com>
2016-05-02 16:41:35 -0700

    Added movement to paddles, keylisteners to Pong class

--- src/pong/Paddle.java
+++ src/pong/Paddle.java
@@ -1,20 +1,45 @@
 package pong;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 public class Paddle {
 
 	public int paddleNumber;
 	
-	public int x, y, width = 100, height = 500;
+	public int x, y, width = 50, height = 300;
+	public int score;
 	
-	public Paddle(int paddleNumb) {
+	public Paddle(Pong pong, int paddleNumb) {
 		this.paddleNumber = paddleNumb;
 		if (paddleNumber == 1) {
-			this.x = width;
-			this.y = Pong.pong.height / 2 - this.height / 2;
+			this.x = 0;
 		} else {
-			this.x = Pong.pong.width - width;
-			this.y = Pong.pong.height / 2 - this.height / 2;
+			this.x = pong.width - width;
 		}
+		this.y = pong.height / 2 - this.height / 2;
 		
+	}
+
+	public void render(Graphics g) {
+		g.setColor(Color.WHITE);
+		g.fillRect(x,  y,  width,  height);		
+	}
+
+	public void move(boolean up) {
+		int speed = 15;
+		if (up) {
+			if (y  - speed > 0) {
+				y -= speed;
+			} else {
+				y = 0;
+			}
+		} else {
+			if (y + height + speed < Pong.pong.height) {
+				y += speed;
+			} else {
+				y = Pong.pong.height - height - 15;
+			}
+		}
 	}
 }
--- src/pong/Pong.java
+++ src/pong/Pong.java
@@ -1,36 +1,65 @@
 package pong;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
-public class Pong implements ActionListener{
+public class Pong implements ActionListener, KeyListener{
 
 	public static Pong pong;
 	public int width = 700, height = 700;
 	public Renderer renderer;
+	public Paddle player1;
+	public Paddle player2;
+	
+	public boolean bot = false;
+	public boolean w, s, up, down;
 	
 	public Pong() {
 		Timer timer = new Timer(20, this);
 		JFrame jframe = new JFrame("Pong");
 		renderer = new Renderer();
-		jframe.setSize(width, height);
+		jframe.setSize(width+15, height);
 		jframe.setVisible(true);
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.add(renderer);
+		jframe.addKeyListener(this);
+		
+		start();
 		timer.start();
+	}
+
+	public void start() {
+		player1 = new Paddle(this, 1);
+		player2 = new Paddle(this, 2);
 	}
 	
 	public void update() {
-		
+		if (w) {
+			player1.move(true);
+		}
+		if (s) {
+			player1.move(false);
+		}
+		if (up) {
+			player2.move(true);
+		}
+		if (down) {
+			player2.move(false);
+		}
 	}
 	
 	public void render(Graphics g) {
-		// TODO Auto-generated method stub
-		
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, width, height);
+		player1.render(g);
+		player2.render(g);
 	}
 	
 	public static void main(String[] args){
@@ -42,6 +71,48 @@ public class Pong implements ActionListener{
 	public void actionPerformed(ActionEvent arg0) {
 		update();
 		renderer.repaint();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int id = e.getKeyCode();
+		
+		if (id == KeyEvent.VK_W) {
+			w = true;
+		}
+		if (id == KeyEvent.VK_S) {
+			s = true;
+		}
+		if (id == KeyEvent.VK_UP) {
+			up = true;
+		}
+		if (id == KeyEvent.VK_DOWN) {
+			down = true;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		int id = e.getKeyCode();
+		
+		if (id == KeyEvent.VK_W) {
+			w = false;
+		}
+		if (id == KeyEvent.VK_S) {
+			s = false;
+		}
+		if (id == KeyEvent.VK_UP) {
+			up = false;
+		}
+		if (id == KeyEvent.VK_DOWN) {
+			down = false;
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
